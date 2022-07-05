8c6b2153986e4fcd6e8000cb93c921e396357fd6
nick <nikitashel7@gmail.com>
2022-07-05 15:38:56 +0300

    Initial Commit

---
+++ struct_sorting/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  struct_sorting
+//
+//  Created by Никита Шелудько on 05.07.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
